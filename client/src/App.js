1ae376568c41a7419b74f8c2ce0169b09a87d47c
mathisGuerin <mathis.guerin@idvroom.com>
2019-05-20 14:52:49 +0200

    Separate client and server

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  // Retrieves the list of items from the Express app
+  getList = () => {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(list => this.setState({ list }))
+  }
+
   render() {
     return (
       <div className="App">
